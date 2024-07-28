e6e3e7c27aecdc0ce90f044c3fb1fe71bfbb86b4
Pushan T <pushan.sahanaganesh@gmail.com>
2024-07-28 13:53:11 +0530

    Chatbox skeleton added

---
+++ src/app/chatbot-box.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useState } from "react";
+
+export default function ChatbotBox() {
+    const [Open, setOpen] = useState(false);
+
+    return (
+        <>
+            {!Open ? (
+                <div className="m-8">
+                    <Button onClick={() => setOpen(true)}>Open Chatbot</Button>
+                </div>
+            ) : (
+                <div className="m-8 w-80 h-100 border-2">
+                    <Button onClick={() => setOpen(false)}>×</Button>
+                    <div className="mt-4">
+                        <ScrollArea className="h-full w-full">Text</ScrollArea>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
